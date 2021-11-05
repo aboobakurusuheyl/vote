d1e5fb10c7a1b897a422d5caf841fe3a3df932bb
Mohamed Niyaaz <xaayin@gmail.com>
2021-11-05 16:26:01 +0500

    layout setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    purge: [
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.js",
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/aspect-ratio"),
+    ],
+};
