1df55452be315a7486a4c808e28a26282949e722
Mohamed Niyaaz <xaayin@gmail.com>
2021-11-16 18:26:49 +0500

    fix

--- resources/views/page/institutions-deatail.blade.php
+++ resources/views/page/institutions-deatail.blade.php
@@ -2,7 +2,7 @@
     <div class="relative px-4 sm:px-6 lg:px-8 pb-12">
         <div class="text-lg max-w-prose mx-auto">
             <h1>
-                <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">Approved instituion</span>
+                <span class="block text-base text-center text-cyan-600 font-semibold tracking-wide uppercase">Approved instituion</span>
                 <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Approved Local Courses</span>
             </h1>
         </div>
@@ -40,7 +40,7 @@
             <div class="sm:hidden">
                 <label for="tabs" class="sr-only">Select a tab</label>
                 <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-                <select id="tabs" name="tabs" class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                <select id="tabs" name="tabs" class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-cyan-500 focus:border-cyan-500 sm:text-sm rounded-md">
                     <option>Applied</option>
 
                     <option>Phone Screening</option>
@@ -55,11 +55,11 @@
             <div class="hidden sm:block">
                 <div class="border-b border-gray-200">
                     <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-                        <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200" -->
+                        <!-- Current: "border-cyan-500 text-cyan-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200" -->
                         <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm">
                             Level 1
 
-                            <!-- Current: "bg-indigo-100 text-indigo-600", Default: "bg-gray-100 text-gray-900" -->
+                            <!-- Current: "bg-cyan-100 text-cyan-600", Default: "bg-gray-100 text-gray-900" -->
                             <span class="bg-gray-100 text-gray-900 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">52</span>
                         </a>
 
@@ -69,10 +69,10 @@
                             <span class="bg-gray-100 text-gray-900 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">6</span>
                         </a>
 
-                        <a href="#" class="border-indigo-500 text-indigo-600 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm" aria-current="page">
+                        <a href="#" class="border-cyan-500 text-cyan-600 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm" aria-current="page">
                             Level 3
 
-                            <span class="bg-indigo-100 text-indigo-600 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">4</span>
+                            <span class="bg-cyan-100 text-cyan-600 hidden ml-3 py-0.5 px-2.5 rounded-full text-xs font-medium md:inline-block">4</span>
                         </a>
 
                         <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-200 whitespace-nowrap flex py-4 px-1 border-b-2 font-medium text-sm">
--- resources/views/page/mnqf.blade.php
+++ resources/views/page/mnqf.blade.php
@@ -31,16 +31,16 @@
         <div class="relative px-4 sm:px-6 lg:px-8">
             <div class="text-lg max-w-prose mx-auto">
                 <h1>
-                    <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">{{$data['data']['legend']}}</span>
+                    <span class="block text-base text-center text-cyan-600 font-semibold tracking-wide uppercase">{{$data['data']['legend']}}</span>
                     <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">{{$data['data']['title']}}</span>
                 </h1>
                 <!-- <p class="mt-8 text-xl text-gray-500 leading-8">Today, the Maldivian post-secondary education sector is growing rapidly, with an increasing number of private providers entering the sector to meet the demand. Emerging from this growing importance of post-secondary education is the concomitant requirement to assure the quality of qualifications awarded in testimony of educational attainments. Thus, the Maldives National Qualifications Framework (MNQF) is designed to:</p> -->
 
             </div>
-            <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+            <div class="mt-6 prose prose-cyan prose-lg text-gray-500 mx-auto">
                 {!! $data['data']['content'] !!}
             </div>
-            <!-- <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+            <!-- <div class="mt-6 prose prose-cyan prose-lg text-gray-500 mx-auto">
                 <ul role="list">
                     <li>Facilitate the development of a quality assurance mechanism for the post-secondary education sector.</li>
                     <li>Provide a framework for recognition of qualifications offered in the Maldives and abroad.</li>
