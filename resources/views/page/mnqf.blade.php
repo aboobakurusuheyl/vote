274822c7531d659d1c78a291c118e4c05892918e
Mohamed Niyaaz <xaayin@gmail.com>
2021-11-25 08:43:55 +0500

    fix navigation and added menu

--- resources/views/page/mnqf.blade.php
+++ resources/views/page/mnqf.blade.php
@@ -60,30 +60,24 @@
 
                 <dl class="mt-6 space-y-6 divide-y divide-gray-200">
                     @foreach($data['data']['faq'] as $key=> $value)
-                    <div class="pt-6">
+                    <div class="pt-6" x-data={open:false}>
 
                         <dt class="text-lg">
-                            <!-- Expand/collapse question button -->
-                            <button type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
+
+                            <button type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false" x-on:click="open=!open">
                                 <span class="font-medium text-gray-900">
                                     {{$key}}
                                 </span>
                                 <span class="ml-6 h-7 flex items-center">
-                                    <!--
-                  Expand/collapse icon, toggle classes based on question open state.
 
-                  Heroicon name: outline/chevron-down
-
-                  Open: "-rotate-180", Closed: "rotate-0"
-                -->
-                                    <svg class="rotate-0 h-6 w-6 transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <svg class="rotate-0 h-6 w-6 transform  transition-all" :class="{'-rotate-180':open,'rotate-0':!open}" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
                                     </svg>
                                 </span>
                             </button>
                         </dt>
 
-                        <dd class="mt-2 pr-12" id="faq-0">
+                        <dd class="mt-2 pr-12" id="faq-0" x-show="open">
                             <p class="text-base text-gray-500">
                                 {{$value}}
                             </p>
@@ -92,40 +86,10 @@
                     </div>
                     @endforeach
 
-                    <!-- More questions... -->
+
                 </dl>
 
-                <dl class="mt-6 space-y-6 divide-y divide-gray-200">
-                    <div class="pt-6">
-                        <dt class="text-lg">
-                            <!-- Expand/collapse question button -->
-                            <button type="button" class="text-left w-full flex justify-between items-start text-gray-400" aria-controls="faq-0" aria-expanded="false">
-                                <span class="font-medium text-gray-900">
-                                    MNQF Implementation Notes
-                                </span>
-                                <span class="ml-6 h-7 flex items-center">
-                                    <!--
-                  Expand/collapse icon, toggle classes based on question open state.
 
-                  Heroicon name: outline/chevron-down
-
-                  Open: "-rotate-180", Closed: "rotate-0"
-                -->
-                                    <svg class="rotate-0 h-6 w-6 transform" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
-                                    </svg>
-                                </span>
-                            </button>
-                        </dt>
-                        <dd class="mt-2 pr-12" id="faq-0">
-                            <p class="text-base text-gray-500">
-                                I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.
-                            </p>
-                        </dd>
-                    </div>
-
-                    <!-- More questions... -->
-                </dl>
             </div>
         </div>
     </div>
