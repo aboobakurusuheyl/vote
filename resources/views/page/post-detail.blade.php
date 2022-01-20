995a6f26920ce1ab1ce6d81bb8f3ffb16ec351a8
Mohamed Niyaaz <xaayin@gmail.com>
2022-01-20 08:34:26 +0500

    new changes

--- resources/views/page/post-detail.blade.php
+++ resources/views/page/post-detail.blade.php
@@ -4,10 +4,12 @@
 
             <div class="max-w-7xl mx-auto ">
                 <div class="relative shadow-xl sm:rounded-2xl sm:overflow-hidden  min-h-[460px]">
+                    @if(count($post->getMedia('feature_image'))>0)
                     <div class="absolute inset-0">
                         <img class="h-full w-full object-cover" src="{{$post->getMedia('feature_image')->first()->getFullUrl()}}" alt="People working on laptops">
                         <div class="absolute inset-0 bg-cyan-700 mix-blend-multiply"></div>
                     </div>
+                    @endif
                     <div class="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
                         <h1 class="text-center text-4xl font-extrabold tracking-tight sm:text-5xl lg:text-6xl">
                             <span class="block text-white">{{$post->title}}</span>
