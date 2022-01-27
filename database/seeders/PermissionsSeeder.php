81a95c3f4b0e525ea4708943b02bd36dabf5dfc5
Mohamed Niyaaz <xaayin@gmail.com>
2022-01-27 08:46:00 +0500

    new changeS

--- database/seeders/PermissionsSeeder.php
+++ database/seeders/PermissionsSeeder.php
@@ -18,44 +18,44 @@ class PermissionsSeeder extends Seeder
     public function run()
     {
         // Reset cached roles and permissions
-        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        // app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
-        \DB::table('model_has_permissions')->delete();
-        \DB::table('model_has_roles')->delete();
-        \DB::table('role_has_permissions')->delete();
-        \DB::table('roles')->delete();
-        \DB::table('permissions')->delete();
+        // \DB::table('model_has_permissions')->delete();
+        // \DB::table('model_has_roles')->delete();
+        // \DB::table('role_has_permissions')->delete();
+        // \DB::table('roles')->delete();
+        // \DB::table('permissions')->delete();
 
-        $models = ['user', 'course'];
+        // $models = ['user', 'course'];
 
-        foreach ($models as $model) {
-            Permission::create(['name' => $model . '-view-all']);
-            Permission::create(['name' => $model . '-view']);
-            Permission::create(['name' => $model . '-create']);
-            Permission::create(['name' => $model . '-update']);
-            Permission::create(['name' => $model . '-delete']);
-            Permission::create(['name' => $model . '-restore']);
-        }
+        // foreach ($models as $model) {
+        //     Permission::create(['name' => $model . '-view-all']);
+        //     Permission::create(['name' => $model . '-view']);
+        //     Permission::create(['name' => $model . '-create']);
+        //     Permission::create(['name' => $model . '-update']);
+        //     Permission::create(['name' => $model . '-delete']);
+        //     Permission::create(['name' => $model . '-restore']);
+        // }
 
 
-        $administrator = Role::create(['name' => 'Administrator']);
+        // $administrator = Role::create(['name' => 'Administrator']);
 
-        foreach ($models as $model) {
-            $administrator->givePermissionTo($model . '-view-all');
-            $administrator->givePermissionTo($model . '-view');
-            $administrator->givePermissionTo($model . '-create');
-            $administrator->givePermissionTo($model . '-update');
-            $administrator->givePermissionTo($model . '-delete');
-            $administrator->givePermissionTo($model . '-restore');
-        }
+        // foreach ($models as $model) {
+        //     $administrator->givePermissionTo($model . '-view-all');
+        //     $administrator->givePermissionTo($model . '-view');
+        //     $administrator->givePermissionTo($model . '-create');
+        //     $administrator->givePermissionTo($model . '-update');
+        //     $administrator->givePermissionTo($model . '-delete');
+        //     $administrator->givePermissionTo($model . '-restore');
+        // }
 
 
 
         $user = User::find(1);
-        $user->assignRole($administrator);
+        $user->assignRole('Administrator');
 
         $user_two = User::find(2);
-        $user_two->assignRole($administrator);
+        $user_two->assignRole('Administrator');
 
 
         //
